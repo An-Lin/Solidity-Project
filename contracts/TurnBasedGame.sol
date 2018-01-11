22d70d20f0583d3e8330603d930b2d45be7a9643
njpaterno <npaterno@gmail.com>
2018-01-11 12:30:55 -0500

    TurnBasedGame.sol added

---
+++ contracts/TurnBasedGame.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.18;
+
+contract TurnBasedGame {
+    enum GameState { Begin, Locked, Over }
+    mapping(uint => Game) private unMatchedGames;
+    mapping(address => uint) private addressToGameId;
+    mapping(uint => Game) private gameIdToGame;
+    uint private gamesPlayed = 0;
+
+    function TurnBasedGame() public {
+
+    }
+
+    event GameHasBegun(uint gameId);
+    event GameHasEnded(string winnerAlias, uint gameJackPot);
+
+    struct Player {
+        address player1;
+        string player1Alias;
+    }
+
+    struct Game {
+        Player[] players;
+        GameState state;
+        uint jackpot;
+        uint id;
+    }
+
+
+    function startGame(string name) internal {
+        require(msg.value > 0);
+        /*
+        TODO - Locks
+        */
+        Game memory game;
+        gameIdToGame[gamesPlayed] = game;
+        gameIdToGame[gamesPlayed].players.push(Player(msg.sender, name));
+        addressToGameId[msg.sender] = gamesPlayed;
+    }
+
+    function getGameId() internal view returns (uint){
+        uint id = addressToGameId[msg.sender];
+        require(id != 0);
+        return id;
+    }
+
+    function getGame() internal view returns (Game){
+        return gameIdToGame[getGameId()];
+    }
+
+    function test() internal {
+        getGame().players.push(Player(msg.sender, "name"));
+    }
+}
