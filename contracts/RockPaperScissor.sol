998d19290ecee0052d0dc491348dcd6a15b0a3d4
An Lin <linan2332@gmail.com>
2018-01-11 13:39:59 -0800

    I propose we have a storage list UnmatchGameId that store any gameId that still need opponent

--- contracts/RockPaperScissor.sol
+++ contracts/RockPaperScissor.sol
@@ -1,60 +1,46 @@
-//I design this contract to be first come first play, player do not get to choose who he is playing against. This will probably work but let me know if you think there is better way to implment this
-
 pragma solidity ^0.4.18;
 
 import './TurnBasedGame.sol';
 
 contract RockPaperScissor is TurnBasedGame {
-
-	//player construct
-    struct PlayerStruct {
-        bytes32 encryptedOption;
-        address addr;
-    }
-
-    //delcare two player
-    PlayerStruct public player_one;
-    PlayerStruct public player_two;
-
-
+    
+    uint[] private UnmatchGameId;
+    
 	function RockPaperScissor() public{
-	    reset();
 	}
 
-	function play(bytes32 _encryptedOption) public {
-	    //Scenario #1 First player enter the game
-	    if(player_one.addr == address(0) && player_two.addr == address(0)){
-	        player_one.addr=msg.sender;
-	        player_one.encryptedOption = _encryptedOption;
-	    }
-	    //Scenario #2 Second player join in, signal the player to reveal
-	    else{
-	        player_two.addr=msg.sender;
-	        player_two.encryptedOption = _encryptedOption;
+	function play(bytes32 _encryptedOption, string _name) payable public {
+	    //some basic check
+	    require(msg.value > 0);
+	    require(_encryptedOption!="");
+	    
+		//first check if there is existing game that need opponenet
+		if(UnmatchGameId.length>0){
+		    
+		    //find out the game Id from list of UnmatchGameId
+		    //copy the last unmatchgameId then delete the last it from UnmatchGameId list
+		    uint id = UnmatchGameId[UnmatchGameId.length-1];
+		    delete UnmatchGameId[UnmatchGameId.length-1];
+		    
+		    //player 2 join game and execute game
+		    gameIdToGame[id].players.push(Player(msg.sender, _name));
+		    execute_RockPaperScissor(id);
 
-	        //Event broadcast player can reveal now
-	    }
+		}
+		//if no avaliable game to join, start a fresh game
+		else{
+		     startGame(_name);
+		}
+
 	}
 
 	function reveal(bytes32 _encryptedOption) public {
-
-		//first check if both player properly populated with address and encryptedOption
-		/*require();*/
+        require(_encryptedOption!="");
 
 	    //decode the secret option
 
 	    //compare the option and determin winner
 
 	    //brodcast the result
-	    /*event();*/
-
-	    reset();
-
-	}
-
-	//We call this function to reset the state of the contract
-	function reset() private{
-	    player_one = PlayerStruct("",address(0));
-	    player_two = PlayerStruct("",address(0));
 	}
 }
