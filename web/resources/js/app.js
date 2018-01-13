0f5af91fc159d182794cbb80804e73240c3e7831
njpaterno <npaterno@gmail.com>
2018-01-13 16:53:23 -0500

    UI mostly finished for v1, starting contract calls

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,39 +1,103 @@
-App = {
-  web3Provider: null,
-  contracts: {},
+var App = (function() {
+    var UIController = (function() {
+        const MINIMUM_PASSWORD_LENGTH = 3; // CHANGE FOR PRODUCTION
+        const MINIMUM_USERNAME_LENGTH = 3; // CHANGE FOR PRODUCTION
+        var $nonce = $("#nonce");
+        var $username = $("#username");
+        var $playButton = $("#play-button");
 
-  init: function() {
-    console.log("Initializing Web App.");
-    return App.initWeb3();
-  },
-  initWeb3: function() {
-    console.log("Connecting to Web3 Provider..");
-    // Initialize web3 and set the provider
-    HelperUtil.initWeb3(App);
-    return App.initContract();
-  },
+        function attachUIListeners() {
 
-  initContract: function() {
-    console.log("Instantiating Smart Contract Artifact...");
-    $.getJSON('/RockPaperScissor.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract.
-      var RockPaperScissorArtifact = data;
-      App.contracts.RockPaperScissor = TruffleContract(RockPaperScissorArtifact);
+        }
 
-      // Set the provider for our contract.
-      App.contracts.RockPaperScissor.setProvider(App.web3Provider);
+        return {
+            init: function() {
+                attachUIListeners();
+                this.clear();
+            },
+            clear: function() {
+                $nonce.val("");
+            },
+            validPassword: function(){
+                var result = $nonce.val().length > MINIMUM_PASSWORD_LENGTH;
+                if (result) {
+                    return true;
+                }
+                alert("Please choose a password!");
+                return false;
+            },
+            validUsername: function(){
+                var result = $username.val().length > MINIMUM_USERNAME_LENGTH;
+                if (result) {
+                    return true;
+                }
+                alert("Please choose a username!");
+                return false;
+            },
+            userCanPlay: function() {
+                return this.validPassword() && this.validUsername();
+            },
+            attachPlayButtonClickListener: function(func){
+                $playButton.click(func);
+            }
+        }
+    })()
 
-      return App.getTestNum();
-    });
+    return {
+        web3Provider: null,
+        contracts: {},
 
-    return App.bindEvents();
-  },
+        init: function() {
+            console.log("Initializing Web App.");
+            return App.initWeb3();
+        },
+        initWeb3: function() {
+            console.log("Connecting to Web3 Provider..");
+            // Initialize web3 and set the provider
+            HelperUtil.initWeb3(App);
+            return App.initContract();
+        },
 
-  bindEvents: function() {
-      
-  },
-}
+        initContract: function() {
+            console.log("Instantiating Smart Contract Artifact...");
+            $.getJSON('/RockPaperScissor.json', function(data) {
+                // Get the necessary contract artifact file and instantiate it with truffle-contract.
+                var RockPaperScissorArtifact = data;
+                App.contracts.RockPaperScissor = TruffleContract(RockPaperScissorArtifact);
+
+                // Set the provider for our contract.
+                App.contracts.RockPaperScissor.setProvider(App.web3Provider);
+
+                return App.getTestNum();
+            });
+
+            return App.bindEvents();
+        },
+        play: function(){
+            if (UIController.userCanPlay()) {
+                //AJAX call
+                console.log("User playing game..");
+
+                var RockPaperScissorInstance;
+
+                App.contracts.RockPaperScissor.deployed().then(function(instance) {
+                    RockPaperScissorInstance = instance;
+
+                    return RockPaperScissorInstance.play();
+                }).then(function(result) {
+                    num = result.c[0];
+                }).catch(function(err) {
+                    console.log(err.message);
+                });
+            }
+        },
+        bindEvents: function() {
+            UIController.init();
+            UIController.attachPlayButtonClickListener(this.play());
+        },
+    }
+})()
 
 $(window).on('load', function() {
-  App.init();
+    App.init();
 });
